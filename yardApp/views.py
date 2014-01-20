5ec3eaba4b78984c838d86923f6a0eef69b34d8a
zhangtaoqd <zhangtao@penavicogw.com>
2014-01-20 12:34:42 +0800

    2014-01-20 12:34

--- yardApp/views.py
+++ yardApp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader,RequestContext
+
+from yardApp import models
 # Create your views here.
 def client_index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+
+    template = loader.get_template("yard/clientindex.html")
+    context = RequestContext(request)
+    return HttpResponse(template.render(context))
+def clientquery(request):
+    return HttpResponse("客户查询")
