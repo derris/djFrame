0af04b96b9f420d6b93adc14eac6b832fde193d3
derris <derris@163.com>
2014-01-23 21:20:38 +0800

    this is a test ,just change nothing .

--- yard/settings.py
+++ yard/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# this is a test from donghai to test update file 4 settings .
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
