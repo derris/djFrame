6ea891aa72c687ad12854c4077fea71677eb1011
zhangtaoqd <zhangtao@penavicogw.com>
2014-02-20 14:00:19 +0800

    2014-02-19 09:13

--- yardApp/views.py
+++ yardApp/views.py
@@ -60,25 +60,6 @@ def clients(request):
                                                 'recTim':recTimObj,
                                                 'remark':remarkObj})
 
-#@require_http_methods(["GET", "POST"])
-@csrf_exempt
-def getClients(request):
-    #return HttpResponse(serializers.serialize('json',models.Client.objects.all(),ensure_ascii = False))
-    d = json.loads(serializers.serialize('json',
-        models.Client.objects.raw("select id,client_name,"
-                                  "case client_flag when true then 'true' else 'false' end client_flag,"
-                                  "case custom_flag when true then 'true' else 'false' end custom_flag,"
-                                  "case ship_corp_flag when true then 'true' else 'false' end ship_corp_flag,"
-                                  "case yard_flag when true then 'true' else 'false' end yard_flag,"
-                                  "case port_flag when true then 'true' else 'false' end port_flag,"
-                                  "case financial_flag when true then 'true' else 'false' end financial_flag,"
-                                  "remark from c_client"),ensure_ascii = False))
-    t = []
-    for item in d:
-        item['fields']['id'] = item['pk']
-        t.append(item['fields'])
-    return HttpResponse(json.dumps({'total':2,'rows':t},ensure_ascii = False))
-    #return HttpResponse('321')
 
 @csrf_exempt
 @transaction.atomic
--- yardApp/yardurls.py
+++ yardApp/yardurls.py
@@ -9,19 +9,20 @@ urlpatterns = patterns('',
     url(r'^maintab/$',views.maintab,name='maintab'),
     url(r'^commonsearch/$',views.getCommonSearchTemplate,name='commonsearchtemplate'),
     url(r'^clients/$',views.clients,name='clients'),
-    url(r'^clients/getclients/$',views.getClients,name='getclients'),
+    url(r'^clients/getclients2/$',views.getclients2,name='getclients2'),
     url(r'^clients/updateclients/$',views.updateClients,name='updateclients'),
+    url(r'^sysdata/syscod/$',views.syscod,name='syscod'),
+    url(r'^sysdata/getsyscod/$',views.getsyscod,name='getsyscod'),
+
     # 实验室
     url(r'^lab/$',"yardApp.lab.index", name='labindex'),
     url(r'^lab/getfunc/$',"yardApp.lab.getfunc", name='labgetfunc'),
     url(r'^lab/post/$',"yardApp.lab.getJsonPost", name='labpost'),
     # http://127.0.0.1:8000/yard/lab/getfunc/?func=getJson1&&args=%22aaa%22
 
-    url(r'^clients/getclients2/$',views.getclients2,name='getclients2'),
+
     url(r'^clients/getclients3/$',views.getclients3,name='getclients3'),
 
-    url(r'^sysdata/syscod/$',views.syscod,name='syscod'),
-    url(r'^sysdata/getsyscod/$',views.getsyscod,name='getsyscod'),
 
     url(r'^contract/contract/$',views.contract,name='contract'),
 )
