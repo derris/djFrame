a0cb9fa5b608f252927a0dccf226caaee18aeffd
derris <derris@163.com>
2014-02-20 17:51:01 +0800

    完善代码。

--- zdCommon/utils.py
+++ zdCommon/utils.py
@@ -14,6 +14,8 @@ def logger(aMsg, alogFile = BASE_DIR + 'djgw.log'):
     # if os.path.exists(alogFile): raise Exception("our system log file does not exist") 直接写。
 
 '''
+目的：dump任何对象到文件中保存起来。可以load出来。
+使用方法：
 >>> import zdCommon.utils
 >>> zdCommon.utils.dump2file('sdfs', 'aa.txt')
 >>> b = zdCommon.utils.load4file('aa.txt')
