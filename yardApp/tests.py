7c23db03d2f67e2cc779bd14174c27587d0dc12d
derris <derris@163.com>
2014-08-18 18:11:17 +0800

    frameWork

--- yardApp/tests.py
+++ yardApp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+import unittest
+from django.test import Client
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
 
-# Create your tests here.
+class MyTest2(unittest.TestCase):
+    def test_something(self):
+        response = Client().get('/')
+        print(response.content)
+        self.failUnlessEqual('abc', response.content)
+
+if __name__ == '__main__':
+    unittest.main()
